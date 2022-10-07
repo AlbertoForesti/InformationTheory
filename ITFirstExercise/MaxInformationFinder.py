ca26f98785674a51c3165a3d3fd0e8be4899b936
AlbertoForesti <alberto.foresti.01@icloud.com>
2022-10-07 19:10:42 +0200

    Solved white vector bug

--- ITFirstExercise/MaxInformationFinder.py
+++ ITFirstExercise/MaxInformationFinder.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from scipy.optimize import fsolve
-import seaborn as sns
 
 
 def f(n):
@@ -36,14 +35,17 @@ def entropy_plotter_2d():
     fig, ax = plt.subplots( )
     ax.plot( prob, H )
     plt.show( )
+    plt.close( )
 
 
 def max_entropy_vector_plot():
+    path = "C:\\Users\\Gian Luca Foresti\\Desktop\\Materiale Uni\\4 - anno\\IT"
     H = entropy_plot()
     max_vector = np.unravel_index( np.nanargmax( H, axis=None ), H.shape )  # returns a tuple
     plt.scatter(max_vector[0], max_vector[1], color="white", edgecolors="black")
-    plt.title(f"Hmax = {round(H[ max_vector[ 0 ] ][ max_vector[ 1 ] ], 2)} p1 = {max_vector[ 0 ]}% p2 = {max_vector[ 1 ]}%")
-    plt.show( )
+    plt.title(f"Hmax = {round(H[ max_vector[ 0 ] ][ max_vector[ 1 ] ], 2)}, p1 = {round(max_vector[ 0 ]/100., 2)}, p2 = {round(max_vector[ 1 ]/100., 2)}")
+    plt.savefig(path+"\\Ex1A.png")
+    plt.close( )
 
 
 def entropy_plot():
@@ -54,28 +56,28 @@ def entropy_plot():
     plt.xticks(np.arange(0, 101, 10), np.round(np.linspace(0, 1, 11), 2))  # Set text labels and properties.
     plt.yticks( np.arange( 0, 101, 10 ), np.round( np.linspace( 0, 1, 11 ), 2 ) )  # Set text labels and properties.
     plt.imshow( H, cmap='rainbow', interpolation='bilinear' )
-    # sns.heatmap(H)
-    # sns.heatmap(H, xticklabels=np.arange(0, 1, 0.01).round(2), yticklabels=np.arange(0, 1, 0.01).round(2))
     return H
 
 
 def average_vector_entropy_plot():
+    path = "C:\\Users\\Gian Luca Foresti\\Desktop\\Materiale Uni\\4 - anno\\IT"
     entropy_plot()
     p1 = round( random.random( ), 2 )
     p2 = round( random.uniform( 0, 1 - p1 ), 2 )
-    plt.scatter( p1 * 100, p2 * 100, color="hotpink", edgecolors="black" )
     p = (p1 + p2) / 2
-    plt.scatter( p * 100, p * 100, color="red", edgecolors="black" )
+    plt.scatter( p1 * 100, p2 * 100, color="green", edgecolors="black", label="Original vector" )
+    plt.scatter( p * 100, p * 100, color="red", edgecolors="black", label="Averaged vector" )
     plt.title(
         f"p1 = {round(p1, 2)}, p2 = {round(p2, 2)}, p3 = {round(1 - p1 - p2, 2)}, H = {round( fc( p1, p2 ), 2 )}\n"
-        f"p1 = {round(p, 2)}, p2 = {round(p, 2)}, p3 = {round(1 - p1 - p2, 2)}, H = {round( fc( p, p ), 2 )}" )
-    plt.show( )
+        f"p = {round(p, 2)}, p = {round(p, 2)}, p3 = {round(1 - p1 - p2, 2)}, H = {round( fc( p, p ), 2 )}" )
+    plt.legend()
+    plt.savefig(path+"\\Ex1B.png")
+    plt.close()
 
 
 def meal_pmf(costs, average):
-    print(costs)
-    print(np.average(costs))
-    fun = lambda x : sum((costs - average)*(x**costs))/(x**costs[0]) #division by costs[0] to discard trivial solutions
+    path = "C:\\Users\\Gian Luca Foresti\\Desktop\\Materiale Uni\\4 - anno\\IT"
+    fun = lambda x: sum((costs - average)*(x**costs))/(x**costs[0]) #division by costs[0] to discard trivial solutions
     beta = fsolve(fun, 2/costs.shape[0])
     alpha = 1/(sum(beta**costs))
     p = np.asarray([alpha*(beta**c) for c in costs])
@@ -85,6 +87,6 @@ def meal_pmf(costs, average):
     ax.set_title( 'Probability Mass Function' )
     plt.ylim([0, 1])
     plt.scatter(costs, p, c="white", edgecolors="black")
-    #sns.scatterplot(x=costs, y=p)
     plt.grid()
-    plt.show()
+    plt.savefig(path+"\\Ex2A.png")
+    plt.close( )
